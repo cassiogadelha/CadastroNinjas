963bc627d64651dfe56ac55cf77f2a710e377b9b
Cássio Gadêlha <61876421+cassiogadelha@users.noreply.github.com>
2025-03-26 21:13:49 -0300

    Criado a Controller 2

--- src/main/java/dev/java10x/CadastroNinjas/Controller.java
+++ src/main/java/dev/java10x/CadastroNinjas/Controller.java
@@ -1,4 +1,13 @@
 package dev.java10x.CadastroNinjas;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Boas Vindas, my friend";
+    }
 }
